2ee6b682fa01dbffd50d6ae40c1a5b5da4efc1df
Emma <yaoxiuning@126.com>
2023-10-02 00:39:40 +0800

    feat: 新增历史记录 群聊 新建帖子页面

---
+++ pages/my/chatGroups/index.js
@@ -0,0 +1,14 @@
+Page({
+  data: {
+    items: [{
+      name: '跨过进香河',
+      productNum:'115',
+      peopleNum:'360',
+      image:'https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/64e4c32a5a7e3f03100cd3d5/64e5fbe4ab696a00113fcdc0/90ee4d5cb5f73f25d6df78b665c0104b.png'
+    }],
+  },
+
+  onShareAppMessage() {
+    return {};
+  },
+});
